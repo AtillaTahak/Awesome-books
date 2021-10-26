ff26087a54b9df46e1749300de65eaba241936b1
Atilla Taha Kördüğüm <atillatahaa@gmail.com>
2021-10-26 17:10:21 +0300

    added class

--- js/main.js
+++ js/main.js
@@ -2,47 +2,52 @@ const addBooks = document.querySelector('#add');
 const bookName = document.getElementById('title');
 const authorName = document.getElementById('author');
 
+
 let bookList = JSON.parse(localStorage.getItem('books'));
 if (bookList == null) {
   bookList = [];
 }
 
-function createBook(bookElement) {
-  document.querySelector('#book-list').innerHTML = '';
-  bookElement.forEach((e) => {
-    const element = document.createElement('li');
-
-    element.innerHTML += `
-          <p>${e.bookNames}</p>
-          <p>${e.authorNames}</p>
-          <input type="button" value="remove" onclick="removeBook(this.id)" id="${bookElement.length}">
-          <hr>`;
-    document.querySelector('#book-list').appendChild(element);
-  });
-  localStorage.clear();
-  localStorage.setItem('books', JSON.stringify(bookElement));
-}
-
 window.addEventListener('load', () => {
-  createBook(bookList);
+  const other = new BookClass(bookList);
+  other.createBook();
 });
 
 function removeBook(remBookName) {
-  const filterbookList = bookList.filter((e) => e.id !== remBookName - 1);
-  bookList = filterbookList;
-  createBook(bookList);
+  const other = new BookClass(bookList,remBookName);
+  other.removeBookClass();
 }
 
 addBooks.addEventListener('click', () => {
-  let bookListlet = {};
-
-  if (bookList.length > 0) {
-    bookListlet = { id: bookList.length, bookNames: bookName.value, authorNames: authorName.value };
-  } else if (bookList.length < 0) {
-    removeBook(bookList);
-  } else {
-    bookListlet = { id: 0, bookNames: bookName.value, authorNames: authorName.value };
-  }
+  let bookListlet = { id: Math.random().toString(16).slice(2), bookNames: bookName.value, authorNames: authorName.value };
   bookList.push(bookListlet);
-  createBook(bookList);
+  const other = new BookClass(bookList);
+  other.createBook();
 });
+class BookClass {
+  constructor(bookList,removeId) {
+    this.bookList = bookList;
+    this.removeId =removeId;
+  }
+
+  createBook() {
+    document.querySelector('#book-list').innerHTML = '';
+    this.bookList.forEach((e) => {
+      const element = document.createElement('li');
+
+      element.innerHTML += `
+          <p>${e.bookNames}</p>
+          <p>${e.authorNames}</p>
+          <input type="button" value="remove" onclick="removeBook(this.id)" id="${e.id}">
+          <hr>`;
+      document.querySelector('#book-list').appendChild(element);
+    });
+    localStorage.clear();
+    localStorage.setItem('books', JSON.stringify(bookList));
+  }
+  removeBookClass(){
+    const index = this.bookList.findIndex(prop => prop.id === this.removeId)
+    bookList.splice(index, 1)
+    this.createBook();
+  }
+}
